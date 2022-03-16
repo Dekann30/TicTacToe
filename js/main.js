ce0454cdda82c9b3deb173aeb33a100698cb1a0e
Deanna K <keysdeanna@gmail.com>
2022-03-15 23:53:02 -0400

    adds psudo code for game functionality

--- js/main.js
+++ js/main.js
@@ -15,8 +15,16 @@ console.log('JavaScript is here')
 
 /*----- cached element references -----*///- things referenced constantly
 //game squares
+//message container - h1
 //reset button
 
 /*----- event listeners -----*/
+//mouse click on 9 squares
+//mouse click on reset button
+//optional - user initiates game start
 
 /*----- functions -----*/
+//intialize start game
+//handle user interaction (logic for what happens when user interacts)
+//check for 3 in a row -or winner(main game logic)
+//render messages to the dom
