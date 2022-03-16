5c4ac8dabe43a9ec5ee37618e24312b4959c1de3
Deanna K <keysdeanna@gmail.com>
2022-03-15 22:30:48 -0400

    adds game board and squares

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('Js is running')
+console.log('JavaScript is here')
+
+
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
